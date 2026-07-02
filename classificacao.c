f1b9a3ad97ea71ea33c00d8d99e71d324af5bcd4
FranciscoDSMoura <franciscodsmoura@gmail.com>
2026-07-01 22:54:27 -0300

    Update classificacao.c

--- classificacao.c
+++ classificacao.c
@@ -49,41 +49,19 @@ void salvar_arquivo_classificacao(void* bd_t, void* bd_p, const char* caminho_ar
 
 // NOVAS FUNÇÕES:
 
-// Função de comparação seguindo os critérios do TP2
-int comparar_times(const void *a, const void *b) {
-    Time *timeA = *(Time **)a;
-    Time *timeB = *(Time **)b;
+int comparar_times(const void *a, const void *b);
 
-    // 1º Critério
-    if (timeA->pontos_ganhos != timeB->pontos_ganhos) {
-        return timeB->pontos_ganhos - timeA->pontos_ganhos;
-    }
-    // 2º Critério
-    if (timeA->vitorias != timeB->vitorias) {
-        return timeB->vitorias - timeA->vitorias;
-    }
-    // 2º Critério
-    if (timeA->saldo_gols != timeB->saldo_gols) {
-        return timeB->saldo_gols - timeA->saldo_gols;
-    }
-    // 3º Critério
-    if (timeA->gols_m != timeB->gols_m) {
-        return timeB->gols_m - timeA->gols_m;
-    }
-
-    return timeA->id - timeB->id;
-}
-
-void bd_imprimir_tabela_ordenada(void *bd_t, BDPartidas *bd_p) {
-    BDTime *bd = (BDTime *)bd_t;
+void imprimir_tabela_classificacao_ordenada(void *bd_time, void* bd_partida){
+    BDTime *bd_t = (BDTime *)bd_time;
+    BDPartidas *bd_p = (BDPartidas *)bd_partida;
     
     classificacao_completa(bd_t, bd_p);
 
     int num_times = 0;
-    NoTime *atual = bd->inicio;
+    NoTime *atual = recebe_BDTime_inicio(bd_t);
     while (atual) {
         num_times++;
-        atual = atual->proximo;
+        atual = recebe_NoTime_proximo(atual);
     }
     if (num_times == 0) {
         printf("\nNenhum time cadastrado.\n");
@@ -94,23 +72,65 @@ void bd_imprimir_tabela_ordenada(void *bd_t, BDPartidas *bd_p) {
         printf("Erro de alocacao de memoria.\n");
         return;
     }
-    atual = bd->inicio;
+    atual = recebe_BDTime_inicio(bd_t);
     for (int i = 0; i < num_times; i++) {
-        vetor_times[i] = &(atual->time);
-        atual = atual->proximo;
+        vetor_times[i] = recebe_time_do_no(atual);
+        atual = recebe_NoTime_proximo(atual);
     }
     
     qsort(vetor_times, num_times, sizeof(Time *), comparar_times);
 
     printf("Pos  ID   Time            V    E    D    GM   GS   S    PG\n");
-    printf("-------------------------------------------------------------------------\n");
     for (int i = 0; i < num_times; i++) {
         Time *t = vetor_times[i];
-        printf("%-3d  %-3d  %-14s  %-3d  %-3d  %-3d  %-3d  %-3d  %-3d  %-3d\n",
-               i + 1, t->id, t->nome, t->vitorias, t->empates, t->derrotas, 
-               t->gols_m, t->gols_s, t->saldo_gols, t->pontos_ganhos);
+        imprimir_time_com_classificacao(t, i);
     }
     
     free(vetor_times);
 }
 
+void salvar_tabela_ordenada_arquivo(void *bd_time, void *bd_partida, const char* caminho_arquivo){
+    // Abre o arquivo para escrita
+    FILE *arquivo = fopen(caminho_arquivo, "w");
+    if (!arquivo) {
+        printf("Erro ao salvar em %s\n", caminho_arquivo);
+        return;
+    }
+
+    BDTime *bd_t = (BDTime *)bd_time;
+    BDPartidas *bd_p = (BDPartidas *)bd_partida;
+    
+    classificacao_completa(bd_t, bd_p);
+
+    int num_times = 0;
+    NoTime *atual = recebe_BDTime_inicio(bd_t);
+    while (atual) {
+        num_times++;
+        atual = recebe_NoTime_proximo(atual);
+    }
+    if (num_times == 0) {
+        printf("\nNenhum time cadastrado.\n");
+        return;
+    }
+    Time **vetor_times = (Time **)malloc(num_times * sizeof(Time *));
+    if (!vetor_times) {
+        printf("Erro de alocacao de memoria.\n");
+        return;
+    }
+    atual = recebe_BDTime_inicio(bd_t);
+    for (int i = 0; i < num_times; i++) {
+        vetor_times[i] = recebe_time_do_no(atual);
+        atual = recebe_NoTime_proximo(atual);
+    }
+    
+    qsort(vetor_times, num_times, sizeof(Time *), comparar_times);
+
+    fprintf(arquivo, "Pos  ID   Time            V    E    D    GM   GS   S    PG\n");
+    for (int i = 0; i < num_times; i++) {
+        Time *t = vetor_times[i];
+        salvar_time_com_classificacao(t, i, arquivo);
+    }
+    
+    free(vetor_times);
+    fclose(arquivo);
+}
